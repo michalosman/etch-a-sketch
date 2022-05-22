08026326aa9c780c507a11d0e6c97fc3faaa2404
Michał Osman <michal.osman.997@gmail.com>
2022-05-22 17:48:01 +0200

    Fix color picker

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const sizeValue = document.getElementById('sizeValue')
 const sizeSlider = document.getElementById('sizeSlider')
 const grid = document.getElementById('grid')
 
-colorPicker.onchange = (e) => setCurrentColor(e.target.value)
+colorPicker.oninput = (e) => setCurrentColor(e.target.value)
 colorBtn.onclick = () => setCurrentMode('color')
 rainbowBtn.onclick = () => setCurrentMode('rainbow')
 eraserBtn.onclick = () => setCurrentMode('eraser')
@@ -65,7 +65,7 @@ function setupGrid(size) {
 
   for (let i = 0; i < size * size; i++) {
     const gridElement = document.createElement('div')
-    gridElement.classList.add("grid-element")
+    gridElement.classList.add('grid-element')
     gridElement.addEventListener('mouseover', changeColor)
     gridElement.addEventListener('mousedown', changeColor)
     grid.appendChild(gridElement)
